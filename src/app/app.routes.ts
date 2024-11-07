d312d2dcf40364b20c8fd90df17bb125edda5222
Eduardo Lazaro <eduardo.oliveira@ciag.org.br>
2024-11-07 13:08:53 -0300

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { LadingPageComponent } from './lading-page/lading-page.component';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: LadingPageComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'home', component: HomeComponent },
+  { path: '**', component: NotFoundComponent }
+];
